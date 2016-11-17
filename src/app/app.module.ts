2323dd23f216936eb5177ec0cd4661568b74129f
Juan Sebastián Pérez <jsperez@impok.co>
2016-11-17 02:22:23 -0500

    ReadMe file

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PlayerNameComponent } from './player-name/player-name.component';
+import { GameComponent } from './game/game.component';
+import { ResultComponent } from './result/result.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PlayerNameComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'result', component: ResultComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayerNameComponent,
+    GameComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
