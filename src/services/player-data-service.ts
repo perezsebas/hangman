382c289c4e9a3d5dc2558a6d9b64683cead88638
Juan Sebastián Pérez <jsperez@impok.co>
2016-11-18 01:36:27 -0500

    Injectable added

--- src/services/player-data-service.ts
+++ src/services/player-data-service.ts
@@ -1,9 +1,11 @@
 import { PlayerModel } from '../app/player-name/player-model';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class PlayerDataService {
    player: PlayerModel = {
     "id": 1,
-    "name": name,
+    "name": "Sebas",
     "round": 1,
     "moves": 5,
   }
