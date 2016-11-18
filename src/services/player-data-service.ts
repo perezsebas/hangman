af9aa20b8bc565fd363006c419dae4af24259b1c
Juan Sebastián Pérez <jsperez@impok.co>
2016-11-18 01:30:58 -0500

    Player data service created

---
+++ src/services/player-data-service.ts
@@ -0,0 +1,11 @@
+import { PlayerModel } from '../app/player-name/player-model';
+
+export class PlayerDataService {
+   player: PlayerModel = {
+    "id": 1,
+    "name": name,
+    "round": 1,
+    "moves": 5,
+  }
+  return;
+}
