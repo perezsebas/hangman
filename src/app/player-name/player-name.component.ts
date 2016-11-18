95e218c69ae273c7b092e6c6613bbd46cb9b15a9
Juan Sebastián Pérez <jsperez@impok.co>
2016-11-17 23:59:44 -0500

    Player type defined

--- src/app/player-name/player-name.component.ts
+++ src/app/player-name/player-name.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerModel } from './player-model';
 
 @Component({
   selector: 'player-name',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-name.component.css']
 })
 export class PlayerNameComponent implements OnInit {
-  player = {
+  player: PlayerModel = {
+    "id": 1,
     "name": 'Sebas',
     "round": 1,
     "moves": 5,
