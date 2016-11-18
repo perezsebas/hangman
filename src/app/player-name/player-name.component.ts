6429196d92ef77fe8a4b6c71b6d9dea167b99a99
Juan Sebastián Pérez <jsperez@impok.co>
2016-11-18 02:10:35 -0500

    Private playerDataService

--- src/app/player-name/player-name.component.ts
+++ src/app/player-name/player-name.component.ts
@@ -1,31 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { PlayerModel } from './player-model';
+import { Component, OnInit, Input } from '@angular/core';
+//import { PlayerModel } from './player-model';
+import { PlayerDataService } from '../../services/player-data-service';
 
 @Component({
   selector: 'player-name',
+  inputs: ['name'],
   templateUrl: './player-name.component.html',
   styleUrls: ['./player-name.component.css']
 })
 export class PlayerNameComponent implements OnInit {
+  public name: string;
   /*player: PlayerModel = {
     "id": 1,
     "name": 'Sebas',
     "round": 1,
     "moves": 5,
   }*/
-  constructor() { }
+  constructor(private playerDataService: PlayerDataService) { }
 
   ngOnInit() {
   }
 
   start(){
-    let player: PlayerModel = {
+    //console.log(name);
+    /*let player: PlayerModel = {
       "id": 1,
-      "name": 'Sebas',
+      "name": name,
       "round": 1,
       "moves": 5,
-    }
-    console.log(player.name);
+    }*/
+    //let player = this.playerDataService;
+    //console.log(this.playerDataService.player.name);
+    console.log(this.name);
   }
 
 }
