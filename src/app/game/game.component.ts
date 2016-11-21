76c83623563519acf55c28d3c6bd16494e446d7f
Juan Sebastián Pérez <jsperez@impok.co>
2016-11-21 02:38:01 -0500

    Services added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerDataService } from '../../services/player-data-service';
+import { WordsDataService } from '../../services/words-data-service';
+import { GuessLetter } from '../../pipes/guess-letter';
+import { SecretWord } from '../../pipes/secret-word';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
+  providers: [ GuessLetter, SecretWord ]
 })
 export class GameComponent implements OnInit {
+  public player;
+  public word;
+  public wordLetters;
+  public guess;
+  public letters;
 
-  constructor() { }
+  constructor(
+    private playerDataService: PlayerDataService,
+    private wordsDataService: WordsDataService,
+    private pipe: GuessLetter,
+    private secret: SecretWord,
+  ) { }
 
   ngOnInit() {
+    this.player = this.playerDataService.getPlayer();
+    this.word = this.wordsDataService.getWord();
+    this.letters = this.wordsDataService.getLetters();
+    this.wordLetters = this.word.word.split(',');
+    for (let i in this.word.word) {
+        this.wordLetters[i] = {letter: this.word.word[i], chosen: false, none: " _ "};
+    }
+    //console.log(this.wordLetters);
   }
 
+  try (guess) {
+    this.wordsDataService.setTry(guess, true);
+    this.guess = false;
+
+    for (let i in this.word.word) {
+        if (this.wordLetters[i].letter == guess){
+          this.wordLetters[i].chosen = true;
+          this.guess = true;
+        }
+    }
+
+    if(!this.guess){
+      this.playerDataService.setTry();
+    }
+  }
 }
--- src/services/player-data-service.ts
+++ src/services/player-data-service.ts
@@ -1,14 +1,37 @@
 import { PlayerModel } from '../models/player-model';
+import { Player } from '../data/player-data';
 import { Injectable } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Injectable()
 export class PlayerDataService {
-   player: PlayerModel = {
-    "id": 1,
-    "name": "Sebas",
-    "round": 1,
-    "moves": 5,
-    "won": false
+  public router: Router;
+  constructor(router: Router) {
+    this.router = router;
   }
-  return;
+
+  getPlayer(){
+    return Player;
+  }
+
+  setPlayerName(name){
+    for (let i in Player) {
+      //if (Player[i].id == id) {
+        Player[i].name = name;
+      //}
+     }
+  }
+
+  setTry(){
+     for (let i in Player) {
+       //if (Letters[i].letter == guess) {
+         Player[i].moves--;
+         if(Player[i].moves == 0){
+            Player[i].moves = 5;
+           this.router.navigate(['/result']);
+         }
+       //}
+     }
+   }
+
 }
